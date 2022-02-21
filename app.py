bfd5c8885b9dd54470925e26d414bb8b2d0faca3
Kaitlin Berryman <kaitlinberryman@gmail.com>
2022-02-21 16:02:42 -0500

    Add signup route

--- app.py
+++ app.py
@@ -25,5 +25,9 @@ class User(db.Model):
 def index():
     return render_template('index.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
